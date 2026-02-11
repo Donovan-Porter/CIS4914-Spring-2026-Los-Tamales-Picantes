5968d8c17f2ad4204e48389e888ad1ad21f1035a
Valerie Samuels <163075955+vsamuels213@users.noreply.github.com>
2026-02-11 01:31:47 -0500

    handle one vocab file as input

--- minigames/matching.py
+++ minigames/matching.py
@@ -1,12 +1,47 @@
 import random
 import string
+import json
+
+
+class Card:
+    def __init__(self, eng, spn):
+        self.english = eng
+        self.spanish = spn
+        self._is_matched = False
+        self._is_spanish = False
+
+    def compare(self, incoming_word):
+        if self.english == incoming_word or self.spanish == incoming_word:
+            return True
+        return False
+    
+    def change_matched_to_true(self):
+        self._is_matched = True
+
+    def is_matched(self):
+        return self._is_matched
+    
+    def set_spanish_card(self):
+        self._is_spanish = True
+
+    def is_spanish_card(self):
+        return self._is_spanish
+    
+    def get_word(self):
+        if self._is_spanish:
+            return self.spanish
+        return self.english
+    
 
 class MemoryGame:
-    def __init__(self, size=4):
+    def __init__(self, size=4, chapter_num=1):
+        # storing all my current cards
+        self.game_board = list()
+        
         # width & height of board
         self.size = size
 
-        self.cardValues = []
+        self.data_pool = []
         self.total_pairs = 0
 
         # running list of matched cards
@@ -20,12 +55,32 @@ class MemoryGame:
         self.matches_found = 0
 
         # set up the board
-        self.board = self._create_board()
+        self.board = self._create_board(chapter_num)
 
 
-    # create a shuffled board
-    def _create_board(self):
+    def get_string_board_from_game_board(self, game_board):
+        '''
+        Create a 2D list of strings from a 2D list of Card objects
         
+        :param game_board: 2D list of Cards
+
+        '''
+        string_board = []
+        for row in game_board:
+            cur_row = []
+            for cur_card in row:
+                cur_row.append(cur_card.get_word())
+            string_board.append(cur_row)
+        return string_board
+
+
+    def _create_board(self, chapter_num):
+        '''
+        Create a shuffled board
+        
+        :param chapter_num: chapter select
+
+        '''
         # how many pairs are needed?
         self.total_pairs = (self.size * self.size) // 2
 
@@ -39,31 +94,65 @@ class MemoryGame:
             self.matched.append(row)
         
 
-        # test to handle hard size board
-        test = string.ascii_uppercase + string.ascii_lowercase
+        with open("static\\learning-resources\\spn1130-vocab\\vocab1.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
 
-        # only add the amount of letters we need
-        for count in range(self.total_pairs):
-            self.cardValues.append(test[count])
+        groups = data["groups"]
+        for each_group in groups:
+            # get the vocab
+            vocab_list = each_group["vocabulary"]
+            for each_pair in vocab_list:
+                spanish = each_pair["es"]
+                english = each_pair["en"]
+                
+                self.data_pool.append(Card(english,spanish))
+                        
+        # for card in self.data_pool:
+        #     print("English: " + card.english + " || Spanish: " + card.spanish)
 
-        # we need pairs
-        cards = self.cardValues * 2
+        # TODO: how to load the different files -- split on grammar and vocab
 
         # shuffle this stuff
-        random.shuffle(cards)
-
-        # return the shuffled list as a 2D array
-        shuffled_board = []
-        for row_index in range(self.size):
-            start = row_index * self.size
-            end = start + self.size
-
-            row = cards[start:end]
-            shuffled_board.append(row)
+        random.shuffle(self.data_pool)
         
-        return shuffled_board
+        # pick cards
+        temp_board = []
+        for count in range(self.total_pairs):
+            # add eng and span to game board
+            current_card = (self.data_pool[count])
+
+            spanish_card = Card(current_card.english, current_card.spanish)
+            spanish_card.set_spanish_card()
+            english_card = Card(current_card.english, current_card.spanish)
+
+            temp_board.append(spanish_card)
+            temp_board.append(english_card)
+            
+        # shuffle game board
+        random.shuffle(temp_board)
+
+        # make a 2D list of cards
+        cur_idx = 0
+        for row in range(self.size):
+            current_row = []
+            for col in range(self.size):
+                current_row.append((temp_board[cur_idx]))
+                cur_idx += 1
+            self.game_board.append(current_row)
+
+        # get the 2D string board
+        string_board = self.get_string_board_from_game_board(self.game_board)
+
+        return string_board
 
     def click_card(self, row, col):
+        '''
+        Handle a card being clicked
+        Handle comparing the cards clicked
+        
+        :param row: row for the clicked card
+        :param col: col for the clicked card
+        '''
         game_state = None
 
         # ignore clicks on already matched cards
@@ -119,3 +208,4 @@ class MemoryGame:
         
 
         return game_state
+        
