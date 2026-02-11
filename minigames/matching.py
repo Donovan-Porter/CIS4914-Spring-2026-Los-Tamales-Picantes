395dae31a9dc9eb03075788056b4484fbd5082e7
Valerie Samuels <163075955+vsamuels213@users.noreply.github.com>
2026-02-11 01:55:46 -0500

    handle file parsing

--- minigames/matching.py
+++ minigames/matching.py
@@ -35,14 +35,20 @@ class Card:
 
 class MemoryGame:
     def __init__(self, size=4, chapter_num=1):
-        # storing all my current cards
-        self.game_board = list()
+        '''
+        Set up for the matching game
         
+        :param size: size of the board
+        :param chapter_num: chapter to load 
+        '''
         # width & height of board
         self.size = size
 
+        # all the data that is loaded from the file
         self.data_pool = []
+
         self.total_pairs = 0
+        self.game_board = []
 
         # running list of matched cards
         self.matched = []
@@ -56,7 +62,6 @@ class MemoryGame:
 
         # set up the board
         self.string_board = self._create_board(chapter_num)
-
 
     def get_string_board_from_game_board(self, game_board):
         '''
@@ -72,6 +77,74 @@ class MemoryGame:
                 cur_row.append(cur_card.get_word())
             string_board.append(cur_row)
         return string_board
+
+    def get_data_from_file(self, chapter_num, is_vocab):
+        '''
+        Parse the data from the files
+        
+        :param chapter_num: chapter number that was selected
+        :param is_vocab: is this a vocabulary file
+        '''
+        # TODO: handle spanish 1 and spanish 2
+        
+        if is_vocab:
+            self._handle_vocab_file(chapter_num)
+        else:
+            self._handle_grammar_file(chapter_num)
+
+    def _handle_vocab_file(self, chapter_num):
+        '''
+        Parsing the data from a vocab file
+        
+        :param chapter_num: chapter select
+        '''
+        base_vocab_string = "static\\learning-resources\\spn1130-vocab\\"
+
+        filename = base_vocab_string + f"vocab{chapter_num}.json"
+
+        with open(filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        groups = data["groups"]
+        for each_group in groups:
+            # get the vocab
+            vocab_list = each_group["vocabulary"]
+            for each_pair in vocab_list:
+                spanish = each_pair["es"]
+                english = each_pair["en"]
+                
+                self.data_pool.append(Card(english,spanish))
+
+        # for card in self.data_pool:
+        #     print("English: " + card.english + " || Spanish: " + card.spanish)
+
+    def _handle_grammar_file(self, chapter_num):
+        '''
+        Parsing the data from a grammar file
+        
+        :param chapter_num: chapter select
+        '''
+        base_grammar_string = "static\\learning-resources\\spn1130-grammar\\"
+
+        filename = base_grammar_string + f"vocab{chapter_num}.json"
+
+        with open(filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        # TODO: update to handle grammar structure
+
+        # groups = data["groups"]
+        # for each_group in groups:
+        #     # get the vocab
+        #     vocab_list = each_group["vocabulary"]
+        #     for each_pair in vocab_list:
+        #         spanish = each_pair["es"]
+        #         english = each_pair["en"]
+                
+        #         self.data_pool.append(Card(english,spanish))
+
+        for card in self.data_pool:
+            print("English: " + card.english + " || Spanish: " + card.spanish)
 
 
     def _create_board(self, chapter_num):
@@ -92,25 +165,11 @@ class MemoryGame:
             
             # Add the row to the revealed board
             self.matched.append(row)
-        
 
-        with open("static\\learning-resources\\spn1130-vocab\\vocab1.json", "r", encoding="utf-8") as f:
-            data = json.load(f)
-
-        groups = data["groups"]
-        for each_group in groups:
-            # get the vocab
-            vocab_list = each_group["vocabulary"]
-            for each_pair in vocab_list:
-                spanish = each_pair["es"]
-                english = each_pair["en"]
-                
-                self.data_pool.append(Card(english,spanish))
-                        
-        # for card in self.data_pool:
-        #     print("English: " + card.english + " || Spanish: " + card.spanish)
-
-        # TODO: how to load the different files -- split on grammar and vocab
+        # load the file
+        # TODO: test test test
+        is_vocab = True
+        self.get_data_from_file(chapter_num, is_vocab)                    
 
         # shuffle this stuff
         random.shuffle(self.data_pool)
@@ -131,7 +190,7 @@ class MemoryGame:
         # shuffle game board
         random.shuffle(temp_board)
 
-        # make a 2D list of cards
+        # make a 2D list of Card objects
         cur_idx = 0
         for row in range(self.size):
             current_row = []
@@ -187,8 +246,12 @@ class MemoryGame:
         
         return {"result": game_state, "state": self.state()}
 
-    # get the game state for the frontend
+    
     def state(self):
+        '''
+        Get the game state for the frontend  
+
+        '''
         game_state = {}
 
         # get all card values
