9e322f89c8ebf2c42b8ff887815dc0222cda0d98
Valerie Samuels <163075955+vsamuels213@users.noreply.github.com>
2026-04-04 21:57:42 -0400

    updated depend

--- dependencies.bat
+++ dependencies.bat
@@ -1,4 +1,7 @@
 pip install Flask flaskwebgui pyinstaller transformers pyaudio
 pip install python-dotenv
 pip install Pillow
+pip install requests
+pip install torch
+pip install sentencepiece
 py download_model.py
